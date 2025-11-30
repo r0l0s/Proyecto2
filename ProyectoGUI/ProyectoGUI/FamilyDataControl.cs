1f64e81b47d06ef3f76b8e372b7301da72f1202d
tony <ant.rolo98@gmail.com>
2025-11-30 03:06:47 -0600

    feat(interfaz): agrego inicial de interfaz.
    Implementación sencilla de interfaz (formas básicas).
    Falta múltiples partes e implementación con las estructuras de datos.

---
+++ ProyectoGUI/ProyectoGUI/FamilyDataControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoGUI
+{
+    public partial class FamilyDataControl : UserControl
+    {
+        public FamilyDataControl()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateFamilyButton_Click(object sender, EventArgs e)
+        {
+            // Creates a new FamilyTree
+        }
+
+        private void RemoveFamilyButton_Click(object sender, EventArgs e)
+        {
+            // Eliminates FamilyTree
+        }
+
+        private void FamilyConfigButton_Click(object sender, EventArgs e)
+        {
+            // Creates new window for selected family configuration (Adding members, viewing the map and getting the family's statistics)
+        }
+    }
+}
---
+++ ProyectoGUI/ProyectoGUI/MainForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ProyectoGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            familyDataControl1 = new FamilyDataControl();
+            SuspendLayout();
+            // 
+            // familyDataControl1
+            // 
+            familyDataControl1.Location = new Point(252, 9);
+            familyDataControl1.Name = "familyDataControl1";
+            familyDataControl1.Size = new Size(1000, 660);
+            familyDataControl1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(familyDataControl1);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ProyectoFamilia";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FamilyDataControl familyDataControl1;
+    }
+}

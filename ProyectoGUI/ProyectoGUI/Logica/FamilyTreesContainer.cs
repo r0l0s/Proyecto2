56baaed09447b87be491b9db3667eee66adb1e6a
tony <ant.rolo98@gmail.com>
2025-12-02 04:54:50 -0600

    feat: Agrega estructura de árbol para crear familias. - Se implementan las estructuras de datos que forman a un miembro y a una familia.

---
+++ ProyectoGUI/ProyectoGUI/Logica/FamilyTreesContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoGUI.Logica
+{
+    struct FamilyTreeContainer
+    {
+        public static List<FamilyTree> Families = new List<FamilyTree>();
+    }
+}
